18e90968e45f957cc6e95aa557a97615002bc168
ebiryu <ebiryu.zatta@gmail.com>
2016-09-23 10:29:35 +0900

    Finish exercise 3

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,11 +9,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 
-  test "should get signup" do
-    get signup_path
-    assert_response :success
-    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
-  end
 end
